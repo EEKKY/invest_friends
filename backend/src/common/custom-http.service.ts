ec40929dde8049713775471c04d5edff35efc6d6
jihkang <mallangcoogi@gmail.com>
2025-07-12 15:56:27 +0900

    Fix: 매개변수 변수명 수정, 타입 추가

--- backend/src/common/custom-http.service.ts
+++ backend/src/common/custom-http.service.ts
@@ -1,21 +1,25 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosFilterService } from './axios/axios-filter.service';
 
 @Injectable()
 export class CustomHttpService {
   private readonly httpService: HttpService;
 
-  constructor(httpService: HttpService, AxiosFilterService) {
+  constructor(
+    httpService: HttpService,
+    axiosFilterService: AxiosFilterService,
+  ) {
     this.httpService = httpService;
     httpService.axiosRef.interceptors.response.use(
-        response => response,
-        error => {
-            AxiosFilterService.handle(error);
-        }
+      (response) => response,
+      (error) => {
+        axiosFilterService.handle(error);
+      },
     );
   }
 
-  async get<T>(url: string) : Promise<T> {
-    return this.httpService.axiosRef.get(url)
+  async get<T>(url: string): Promise<T> {
+    return this.httpService.axiosRef.get(url);
   }
 }
