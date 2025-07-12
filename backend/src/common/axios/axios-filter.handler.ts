6a44e89e920630f85571c20d6c57df12a0add27b
jihkang <mallangcoogi@gmail.com>
2025-07-12 15:56:55 +0900

    Fix: 테스트 url 변경

--- backend/src/common/axios/axios-filter.handler.ts
+++ backend/src/common/axios/axios-filter.handler.ts
@@ -1,9 +1,9 @@
 export const axiosErrorStrategies: Record<string, any> = {
-  '/login/.*/': (error) => {
-    console.warn('로그인 실패 처리');
+  'https://test.com/.*': (error) => {
+    console.warn('테스트 api 에러 처리');
   },
-  '/payment/.*': (error) => {
-    console.warn('결제 실패 처리');
+  'https://asset.com/.*': (error) => {
+    console.warn('asset api 에러 처리');
   },
   '.*': (error) => {
     console.error(`[DEFAULT] 에러 발생: ${error.config?.url}`);
