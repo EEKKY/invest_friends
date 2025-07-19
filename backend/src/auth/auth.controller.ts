09a1c3aff86679e293ef47beeec4282db1c2b0a8
wj <leewj5192@gmail.com>
2025-07-20 01:32:47 +0900

    feat: 소셜로그인 jwt없이 구현

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,14 +6,133 @@ import {
   Param,
   Patch,
   Post,
+  Req,
+  UseGuards,
+  Res,
 } from '@nestjs/common';
 import { CreateUserInput, UpdateUserDto } from './dto/auth.dto';
-import { AtuhService } from './auth.service';
+import { AuthService } from './auth.service';
 import { AuthEntity } from './entity/auth.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@Controller('user')
+@Controller('auth')
 export class AuthController {
-  constructor(private readonly service: AtuhService) {}
+  constructor(private readonly service: AuthService) {}
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  @ApiOperation({
+    summary: '구글 로그인',
+    description: '구글 로그인을 위한 리다이렉션 API',
+  })
+  @ApiResponse({ status: 302, description: '구글 로그인 페이지로 리다이렉트' })
+  async googleAuth(@Req() req) {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  @ApiOperation({
+    summary: '구글 로그인 콜백',
+    description: '구글 로그인 성공 후 처리',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 프론트엔드로 리다이렉트',
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  async googleAuthRedirect(@Req() req, @Res() res) {
+    const user = await this.service.socialLogin(req);
+    if (user) {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=success`,
+      );
+    } else {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=fail`,
+      );
+    }
+  }
+
+  @Get('naver')
+  @UseGuards(AuthGuard('naver'))
+  @ApiOperation({
+    summary: '네이버 로그인',
+    description: '네이버 로그인을 위한 리다이렉션 API',
+  })
+  @ApiResponse({
+    status: 302,
+    description: '네이버 로그인 페이지로 리다이렉트',
+  })
+  async naverAuth(@Req() req) {}
+
+  @Get('naver/callback')
+  @UseGuards(AuthGuard('naver'))
+  @ApiOperation({
+    summary: '네이버 로그인 콜백',
+    description: '네이버 로그인 성공 후 처리',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 프론트엔드로 리다이렉트',
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  async naverAuthRedirect(@Req() req, @Res() res) {
+    const user = await this.service.socialLogin(req);
+    if (user) {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=success`,
+      );
+    } else {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=fail`,
+      );
+    }
+  }
+
+  @Get('kakao')
+  @UseGuards(AuthGuard('kakao'))
+  @ApiOperation({
+    summary: '카카오 로그인',
+    description: '카카오 로그인을 위한 리다이렉션 API',
+  })
+  @ApiResponse({
+    status: 302,
+    description: '카카오 로그인 페이지로 리다이렉트',
+  })
+  async kakaoAuth(@Req() req) {}
+
+  @Get('kakao/callback')
+  @UseGuards(AuthGuard('kakao'))
+  @ApiOperation({
+    summary: '카카오 로그인 콜백',
+    description: '카카오 로그인 성공 후 유저 정보를 반환합니다.',
+  })
+  @ApiResponse({ status: 200, description: '유저 정보 반환' })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  async kakaoAuthRedirect(@Req() req, @Res() res) {
+    const user = await this.service.socialLogin(req);
+    if (user) {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=success`,
+      );
+    } else {
+      res.redirect(
+        `${this.service.configService.get(
+          'FRONTEND_URL',
+        )}/auth/callback?login=fail`,
+      );
+    }
+  }
 
   @Get()
   findAll(): Promise<AuthEntity[]> {
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthEntity } from './entity/auth.entity';
-import { AtuhService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './strategy/google.strategy';
+import { KakaoStrategy } from './strategy/kakao.strategy';
+import { NaverStrategy } from './strategy/naver.strategy';
+// import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthEntity])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([AuthEntity]),
+    // JwtModule.registerAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (configService: ConfigService) => ({
+    //     secret: configService.get<string>('JWT_SECRET'),
+    //     signOptions: { expiresIn: '1d' },
+    //   }),
+    // }),
+  ],
   controllers: [AuthController],
-  providers: [AtuhService],
+  providers: [AuthService, GoogleStrategy, KakaoStrategy, NaverStrategy],
 })
 export class AuthModule {}
